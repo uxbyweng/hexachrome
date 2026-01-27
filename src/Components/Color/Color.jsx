85150b8255af62efe62da5f1641613bc9a6e9faa
Karsten Weng <karsten@weng.eu>
2026-01-27 18:00:14 +0100

    feat: add color overlay with cancel and delete button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -3,17 +3,30 @@
 import "./Color.css";
 export default function Color({ color, onColorDelete, id }) {
     return (
-        <div className="color" style={{ backgroundColor: color.hex }}>
-            <h2 className="color__hex">{color.hex}</h2>
-            <p className="color__role" style={{ color: color.contrastText }}>
-                <strong>{color.role}</strong>
-            </p>
-            <p className="color__contrast" style={{ color: color.contrastText }}>
-                contrast: {color.contrastText}
-            </p>
-            <button type="button" className="btn btn--delete" onClick={() => onColorDelete(id)} aria-label={`Delete color ${color.hex}`}>
-                ✕
-            </button>
-        </div>
+        <>
+            <div className="color" style={{ backgroundColor: color.hex }}>
+                <div className="overlay hide">
+                    <h2 className="overlay__headline">Really delete this color?</h2>
+                    <div className="overlay__button-group">
+                        <button type="button" className="btn btn--cancel">
+                            Cancel
+                        </button>
+                        <button type="button" className="btn btn--delete">
+                            Delete
+                        </button>
+                    </div>
+                </div>
+                <h2 className="color__hex">{color.hex}</h2>
+                <p className="color__role" style={{ color: color.contrastText }}>
+                    <strong>{color.role}</strong>
+                </p>
+                <p className="color__contrast" style={{ color: color.contrastText }}>
+                    contrast: {color.contrastText}
+                </p>
+                <button type="button" className="btn btn--cross" onClick={() => onColorDelete(id)} aria-label={`Delete color ${color.hex}`}>
+                    ✕
+                </button>
+            </div>
+        </>
     );
 }
