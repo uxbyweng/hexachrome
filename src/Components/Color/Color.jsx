b06fa0f573edd48ee9f41438d905a03690d7c6b3
Karsten Weng <karsten@weng.eu>
2026-01-27 18:12:11 +0100

    feat: add delete functionality for confirm message overlay

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,21 @@
 // Components\Color\Color.jsx
+import { useState } from "react";
 
 import "./Color.css";
 export default function Color({ color, onColorDelete, id }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    <button type="button" onClick={() => setIsOpen(true)}></button>;
     return (
         <>
             <div className="color" style={{ backgroundColor: color.hex }}>
-                <div className="overlay hide">
+                <div className={`overlay${isOpen ? "" : " overlay--hide"}`}>
                     <h2 className="overlay__headline">Really delete this color?</h2>
                     <div className="overlay__button-group">
-                        <button type="button" className="btn btn--cancel">
+                        <button type="button" className="btn btn--cancel" onClick={() => setIsOpen(false)}>
                             Cancel
                         </button>
-                        <button type="button" className="btn btn--delete">
+                        <button type="button" className="btn btn--delete" onClick={() => onColorDelete(id)}>
                             Delete
                         </button>
                     </div>
@@ -23,7 +27,7 @@ export default function Color({ color, onColorDelete, id }) {
                 <p className="color__contrast" style={{ color: color.contrastText }}>
                     contrast: {color.contrastText}
                 </p>
-                <button type="button" className="btn btn--cross" onClick={() => onColorDelete(id)} aria-label={`Delete color ${color.hex}`}>
+                <button type="button" className="btn btn--cross" onClick={() => setIsOpen(true)} aria-label={`Delete color ${color.hex}`}>
                     ✕
                 </button>
             </div>
